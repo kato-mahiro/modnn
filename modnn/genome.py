239d7431919e43565727eaab4baf361389d26fd1
kato-mahiro <katomasahiro10@gmail.com>
2023-07-05 02:04:22 +0900

    configパラメータの追加、有効な結合に関するルールの追加

--- modnn/genome.py
+++ modnn/genome.py
@@ -31,9 +31,10 @@ class Genome:
     def __init__(self, config_path):
         self.config = read_config_file(config_path)
         self.input_num = self.config['INPUT_NUM']
-        self.hidden_num = self.config['HIDDEN_NUM']
-        self.modulatory_num = self.config['MODULATORY_NUM']
         self.output_num = self.config['OUTPUT_NUM']
+        self.neuron_num = self.config['NEURON_NUM']
+        self.lv1_num = self.config['LV1_MODULATORY_NUM']
+        self.lv2_num = self.config['LV2_MODULATORY_NUM']
         self.connection_num = self.config['CONNECTION_NUM']
         self.has_internal_state = self.config['HAS_INTERNAL_STATE']
         self.max_mod_depth = self.config['MAX_MOD_DEPTH']
@@ -50,6 +51,48 @@ class Genome:
         self.output_neurons = [Neuron(random.uniform(self.min_bias, self.max_bias)) for id in range(self.output_num)]
         total_neuron_num = len(self.input_neurons) + len(self.hidden_neurons) + len(self.modulatory_neurons) + len(self.output_neurons)
         self.connections = [ Connection(random.randint(0, total_neuron_num -1), random.randint(0, total_neuron_num), random.uniform(self.min_weight, self.max_weight)) for i in range(self.connection_num)]
+    
+    #ニューロンidからニューロンの種類を取得する
+    def get_neuron_type(self, neuron_id):
+        if neuron_id < self.input_num:
+            return 'input'
+        elif neuron_id < self.input_num + self.output_num:
+            return 'output'
+        elif neuron_id < self.input_num + self.output_num + self.neuron_num:
+            return 'normal'
+        elif neuron_id < self.input_num + self.output_num + self.neuron_num + self.lv1_num:
+            return 'lv1'
+        else:
+            return 'lv2'
+
+    #結合がルールに則っているかを判定する
+    def is_valid_connection(self, connection):
+
+        in_type = self.get_neuron_type(connection.from_neuron_id)
+        out_type = self.get_neuron_type(connection.to_neuron_id)
+
+        #出力ニューロンは結合元になれない
+        if in_type == 'output':
+            return False
+
+        #入力ニューロンは結合先になれない
+        elif out_type == 'input':
+            return False
+
+        #結合元と結合先が同じニューロンになれない
+        elif connection.from_neuron_id == connection.to_neuron_id:
+            return False
+
+        #Lv.1の修飾ニューロンは通常ニューロン・出力ニューロン以外に結合できない
+        elif in_type == 'lv1' and out_type != 'normal' and out_type != 'output':
+            return False
+
+        #Lv.2の修飾ニューロンはLv.1の修飾ニューロン以外に結合できない
+        elif in_type == 'lv2' and out_type != 'lv1':
+            return False
+        
+        else:
+            return True
 
 if __name__ == '__main__':
     # 設定ファイルのパス
